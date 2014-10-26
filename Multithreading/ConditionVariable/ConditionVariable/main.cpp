674074899ea5098637c0bec7c4e33b9aa9a27ff0
Orn Gunnarsson <musbel@gmail.com>
2014-10-26 23:21:10 +0000

    Created Xcode project.

---
+++ Multithreading/ConditionVariable/ConditionVariable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ConditionVariable
+//
+//  Created by Orn Gunnarsson on 26/10/2014.
+//  Copyright (c) 2014 Orn Gunnarsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main( int argc, const char * argv[] )
+{
+
+    return 0;
+}
